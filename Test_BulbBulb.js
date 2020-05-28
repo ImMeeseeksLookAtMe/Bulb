9504b61f0f86fc83e19f49bfdbe8f7ab1dc5ebbe
Gregor M <gregor.mlakar13@gmail.com>
2020-05-28 08:27:20 +0200

    dodana koda

--- Test_BulbBulb.js
+++ Test_BulbBulb.js
@@ -162,3 +162,136 @@ router.post(
         }
     }
 )
+
+//še ne primer urediteve v Express-u
+
+//router file
+
+const express = require("express");
+const router = express.Router({mergeParams: true});
+const { 
+    getAllWorkspaces, 
+    createWorkspace, 
+    getWorkspace, 
+    deleteWorkspace, 
+    updateWorkspace 
+} = require("../controller/workspace.js");
+const { protect, authorize } = require("../middleware/auth");
+
+//include other resource routers
+const toursRouter = require('./tours')
+const servicesRouter = require('./service')
+
+//Re-route into other resource routers
+router.use('/:tourId/tour', toursRouter);
+router.use('/:workspaceId/service', servicesRouter);
+
+router 
+ .route("/")
+ .get(getAllWorkspaces)
+ .post(protect, createWorkspace)
+
+router
+ .route("/:id")
+ .get(protect, getWorkspace)
+ .put(protect, updateWorkspace)
+ .delete(protect, deleteWorkspace)
+
+
+module.exports = router;
+
+//controller file
+
+//@desc Get 1 Workspaces
+//@route GET /api/v1/workspace/:workspaceId 
+//@acess only logged in user
+
+exports.getWorkspace = asyncHandler(async(req, res, next) => {
+    //check for published workspace
+    let workspace = await db.Workspace.findById(req.params.id);
+
+    if(!workspace) {
+        return next(
+            new ErrorResponse(`Workspace not found. Id ${req.params}`, 404)
+        )
+    }
+
+    if(workspace.user.toString() !== req.user.id && req.user.role !== "admin"){
+        return next(
+            new ErrorResponse(`User ${req.params.id} is not authorized to update this workspace`, 401)
+        )
+    }
+
+
+    res.status(200).json({ 
+        sucess: true, 
+        data: workspace 
+    });
+});
+
+//@desc Create 1 Workspaces
+//@route POST /api/v1/workspace/ 
+//@acess only logged in user
+
+exports.createWorkspace = asyncHandler(async(req, res, next) => {
+    //add user to req.body
+    req.body.user = req.user.id;
+
+    // check for published workspace
+    const publishedWorkspace = await db.Workspace.findOne({user: req.user.id})
+    
+    //If the user is not admin, they van only add 1 workspace
+    if(publishedWorkspace && req.user.role !== "admin"){
+        return next(new ErrorResponse(`User with ID ${req.user.id} is not authorized to create more than 1 Workspace.`, 
+        400)
+        )
+    }
+
+    let workspace = await db.Workspace.create(req.body);
+
+    
+    res.status(201).json({
+        sucess: true,
+        data: workspace
+    });
+});
+
+//midleware 
+
+const asyncHandler = fn => (req, res, next) =>
+Promise
+    .resolve(fn(req, res,next))
+    .catch(next);
+
+module.exports = asyncHandler;
+
+exports.protect = asyncHandler(async (req, res, next) =>{
+    let token;
+
+    if(
+        req.headers.authorization &&
+        req.headers.authorization.startsWith("Bearer")
+    ) {
+        token = req.headers.authorization.split(" ")[1]
+    }
+    console.log(token)
+ /*    else if(req.cookies.token) {
+        token = req.cookies.token
+    } */
+
+    //make sure token exists
+    if(!token) {
+        return next(new ErrorResponse("Not authorized to access this route.", 401))
+    }
+
+    try {
+        //Verify token 
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        req.user = await db.User.findById(decoded.id)
+        
+        next();
+    } catch(err) {
+        return next(new ErrorResponse("Not authorized to access this route.", 401))
+    }
+})
